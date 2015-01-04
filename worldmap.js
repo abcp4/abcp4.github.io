94703e33c51e0f7aeaa944a053dd78cce269eb71
navi1921 <navi1921@gmail.com>
2015-01-03 21:27:00 -0300

    Initial commit

--- worldmap.js
+++ worldmap.js
@@ -5,22 +5,7 @@ var startWord = "SAO LUIS";
 var osmLayer = new OpenLayers.Layer.OSM();
 
 // Heat map + point map
-var matrix1 = new OpenLayers.Layer.WMS("WMS",
-  // Uncomment below to use your local server
-  // "http://localhost:8080/geoserver/wms",
-  "http://geoserver-navi1921.rhcloud.com/awesome/wms",
-  {
-    format: "image/png8",
-    transparent: true,
-    layers: "awesome:matrix_geoescolas,awesome:matrix_geoescolas",
-    styles: "point,heatmap"
-  }, {
-    opacity: 0.6,
-    singleTile: true,
-  });
-  
-  
-  var matrix2 = new OpenLayers.Layer.WMS("WMS",
+var wmsLayer = new OpenLayers.Layer.WMS("WMS",
   // Uncomment below to use your local server
   // "http://localhost:8080/geoserver/wms",
   "http://geoserver-navi1921.rhcloud.com/awesome/wms",
@@ -35,44 +20,16 @@ var matrix1 = new OpenLayers.Layer.WMS("WMS",
   });
 
 // Start with map of startWord
-matrix1.mergeNewParams({viewparams: "word:"+startWord});
-matrix2.mergeNewParams({viewparams: "word:"+startWord});
+wmsLayer.mergeNewParams({viewparams: "word:"+startWord});
 
 // Map with projection into (required when mixing base map with WMS)
 olMap = new OpenLayers.Map({
   projection: "EPSG:900913",
   units: "m",
-  layers: [osmLayer],
+  layers: [wmsLayer, osmLayer],
   center: [-4429687.0, -396947.0],
   zoom: 4
 });
-
-
-//////////////////
-var addMatrix1Layer = function() {
-
-
-if(mapPanel.map.layers.indexOf(matrix1) == -1) {
-            mapPanel.map.addLayer(matrix1);
-
-            }else{
-            mapPanel.map.removeLayer(matrix1);
-
-    }
-    };
-    
-var addMatrix2Layer = function() {
-
-        if(mapPanel.map.layers.indexOf(matrix2) == -1) {
-            mapPanel.map.addLayer(matrix2);
-
-            }else{
-            mapPanel.map.removeLayer(matrix2);
-
-    }
-            
-    };
-/////////////////
 
 // Take in user input, fire an event when complete
 var textField = new Ext.form.TextField({
@@ -81,7 +38,7 @@ var textField = new Ext.form.TextField({
     specialkey: function(field, e) {
       // Only update the word map when user hits 'enter'
       if (e.getKey() == e.ENTER) {
-        matrix2.mergeNewParams({viewparams: "word:"+field.getValue()});
+        wmsLayer.mergeNewParams({viewparams: "word:"+field.getValue()});
       }
     }
   }
@@ -90,14 +47,7 @@ var textField = new Ext.form.TextField({
 // Map panel, with text field embedded in top toolbar
 var mapPanel = new GeoExt.MapPanel({
   title: "OpenGeo Geoescolas Heat Map",
-tbar: [ ["Enter a word to map:", textField],
-     new Ext.Toolbar({
-    items: [
-    {text: 'matrix 1', handler: addMatrix1Layer},
-    {text: 'matrix 2', handler: addMatrix2Layer}
-    
-    ]
-})],
+  tbar: ["Enter a word to map:", textField],
   map: olMap
 });
 
